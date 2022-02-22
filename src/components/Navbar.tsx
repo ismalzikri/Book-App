1b8077f102511474ded9a05a516fdcec97a0c6ae
zikri <zikriismal42@gmail.com>
2022-02-22 16:49:22 +0700

    feat(navbar, seacrh component ): UI design & add image language

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,44 @@
 import styled from "styled-components"
 import { Search } from "./Search"
 
+import Flag from '../assets/img/english.jpg'
+
 const Nav = styled.nav `
+  display: flex;
+  align-items: center;
   position:fixed;
   width: 100%;
+  padding: 0px 5px 5px;
   background: #867AE9;
   box-sizing: border-box;
-  border-radius:0px 0px  8px 8px;  
+  border-radius:0px 0px  15px 15px;  
+`
+
+const Logo = styled.span `
+  margin-bottom: 10px;
+  font-size: 30px;
+  font-weight: 500;
+  color: #fff;
+`
+
+const ImageLang =  styled.figure `
+  img {
+    width: 32px;
+    height: 32px;
+    object-fit: cover;
+    border-radius: 50%;
+    border: 2px solid #fff;  
+  }
 `
 
 export const Navbar = () => {
   return (
     <Nav>
+      <Logo>Booku</Logo>
       <Search />
+      <ImageLang>
+        <img src={Flag} alt="flag-language"  />
+      </ImageLang>
     </Nav>
   )
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,39 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import {HiOutlineSearch} from 'react-icons/hi'
 
 const Wrap = styled.section `
   position : relative;
+  width: 50%;
+  display: flex;
+  margin: 0 auto;
 `
 
 const InputSearch = styled.input `
-  padding: 6px;
+  width: 100%;
+  padding: 10px 50px 10px ;
   font-size: 12px;
-  padding-left: 25px;
+  outline:none;
+  border: 1px solid #fff;
+  border-radius: 10px;
+`
+
+const AnimateIcon = keyframes `
+  from {
+    transform : rotate(0deg)
+  }
+  to {
+    transform: rotate(360deg)
+  }
 `
 
 const IconSearch = styled(HiOutlineSearch) `
   position: absolute;
-  left: 5px;
-  top:6px;
+  left: 10px;
+  top:10px;
+  cursor: pointer;
+  :hover {
+    animation: ${AnimateIcon} 0.8s linear;
+  }
 `
 
 export const Search = () => {
